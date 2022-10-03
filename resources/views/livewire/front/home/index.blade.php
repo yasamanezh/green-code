492a50fc64e226f97914e117239db5d3bb08229c
Sadegh <sad3gh21@gmail.com>
2022-10-03 18:57:56 +0330

    change color

--- resources/views/livewire/front/home/index.blade.php
+++ resources/views/livewire/front/home/index.blade.php
@@ -9,7 +9,8 @@
                 <div class="col-md-6">
                     <h1 class="fontsize48"><span class="color-gradient">گرین کد</span></h1>
                     <p class="lead">توسعه کسب کار شما و طراحی انواع سایت های فروشکاهی ، شرکتی ، شخصی و ..</p>
-                    <p class="lead">کمک به شرکت ها در درک چالش های فناوری خود و ارائه راه حل های متناسب با نیازها، بودجه و برنامه آنها</p>
+                    <p class="lead">کمک به شرکت ها در درک چالش های فناوری خود و ارائه راه حل های متناسب با نیازها، بودجه
+                        و برنامه آنها</p>
                     <div class="spacer-double"></div>
                 </div>
             </div>
@@ -43,7 +44,10 @@
                     <div class="p-sm-30 pb-sm-30 mb-sm-30">
                         <h2>تیم برنامه نویسی <span class="color-gradient">گرین کد</span>
                         </h2>
-                        <p>هدف ما ارائه ی خدمات برتر و در نهایت کسب رضایتمندی کارفرما بوده و این مجموعه به صورت صادقانه کل انرژی خود را به صورت شبانه روزی بر روی پروژه های سفارش شده متمرکز می نماید تا سفارشات کارفرمایان محترم را به نحو احسن در کوتاهترین زمان ممکن به انجام برساند امید است سبک کاری ما و نتیجه ی وب سایت طراحی شده رضایت خاطر شما را کسب نماید اعتماد شما بزرگترین سرمایه ماست.</p>
+                        <p>هدف ما ارائه ی خدمات برتر و در نهایت کسب رضایتمندی کارفرما بوده و این مجموعه به صورت صادقانه
+                            کل انرژی خود را به صورت شبانه روزی بر روی پروژه های سفارش شده متمرکز می نماید تا سفارشات
+                            کارفرمایان محترم را به نحو احسن در کوتاهترین زمان ممکن به انجام برساند امید است سبک کاری ما
+                            و نتیجه ی وب سایت طراحی شده رضایت خاطر شما را کسب نماید اعتماد شما بزرگترین سرمایه ماست.</p>
                         <div class="row mt10 rtl-dir mb20">
                             <div class="col-md-5">
                                 <ul class="ul-style-2">
@@ -83,7 +87,8 @@
                         <ul>
                             <li>
                                 <h4>استراتژی</h4>
-                                <p>در این مرحله با برگزاری جلسات هماهنگی با مشتری، نیازها و درخواست ها بررسی می گردد و یک داکیومنت ایجاد میگردد.</p>
+                                <p>در این مرحله با برگزاری جلسات هماهنگی با مشتری، نیازها و درخواست ها بررسی می گردد و
+                                    یک داکیومنت ایجاد میگردد.</p>
                             </li>
                             <li>
                                 <h4>طراحی</h4>
@@ -91,11 +96,13 @@
                             </li>
                             <li>
                                 <h4>توسعه</h4>
-                                <p>سایت تکمیل شده و جهت تست و دیباگ به تیم تست و تحلیل جهت بررسی عملکرد تحویل داده می شود</p>
+                                <p>سایت تکمیل شده و جهت تست و دیباگ به تیم تست و تحلیل جهت بررسی عملکرد تحویل داده می
+                                    شود</p>
                             </li>
                             <li>
                                 <h4>پشتیبانی</h4>
-                                <p>در این مرحله سایت شما در بستر اینترنت بارگزاری شده است و ما تا پایان عمر سایت شما در کنار شما خواهیم بود.</p>
+                                <p>در این مرحله سایت شما در بستر اینترنت بارگزاری شده است و ما تا پایان عمر سایت شما در
+                                    کنار شما خواهیم بود.</p>
                             </li>
                         </ul>
                     </div>
@@ -103,126 +110,29 @@
             </div>
         </div>
     </section>
-    <section id="section-testimonial" class="no-top">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3 text-center">
-                    <h5 class="s2">Testimonials</h5>
-                    <h2>Trusted by over 5,000 happy customers <span class="id-color">worldwide</span></h2>
-                    <p class="lead">Sit labore et officia eiusmod tempor officia eiusmod dolor exercitation nulla dolore
-                        ut id velit et ut anim.</p>
-                    <div class="spacer-20"></div>
+    <section id="section-banner" class="no-bottom no-top bgcustom"
+             style="background-color: rgb(220 245 233 / 62%)!important; background-size: 100%; background-repeat: no-repeat;">
+        <div class="container rtl-dir" style="background-size: 100%; background-repeat: no-repeat;">
+            <div class="row align-items-center g-custom-x" style="background-size: 100%; background-repeat: no-repeat;">
+                <div class="col-lg-4 d-none d-lg-block d-xl-block  text-center"
+                     style="background-size: 100%; background-repeat: no-repeat;">
+                    <img class="relative img-fluid mt-60" src="images/misc/Call-center-bro.png" alt="">
                 </div>
-                <div class="owl-carousel owl-theme wow fadeInUp" id="testimonial-carousel">
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them best.
-                                </p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/1.jpg"> <span>John, Pixar Studio</span>
-                                </div>
-                            </blockquote>
-                        </div>
+
+                <div class="col-lg-4 col-md-6" style="background-size: 100%; background-repeat: no-repeat;">
+                    <div class="p-sm-30 pb-sm-0 mb-sm-0" style="background-size: 100%; background-repeat: no-repeat;">
+                        <h2><span class="color-gradient">مشاوره رایگان</span></h2>
                     </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/2.jpg">
-                                    <span>Sarah, Microsoft</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/3.jpg">
-                                    <span>Michael, Apple</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/4.jpg">
-                                    <span>Thomas, Samsung</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/1.jpg"> <span>John, Pixar Studio</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/2.jpg">
-                                    <span>Sarah, Microsoft</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/3.jpg">
-                                    <span>Michael, Apple</span>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="de_testi opt-2">
-                            <blockquote>
-                                <h4>Excellent Hosting</h4>
-                                <p>Great support, like i have never seen before. Thanks to the support team, they are
-                                    very helpfull. This company provide customers great solution, that makes them
-                                    best.</p>
-                                <div class="de_testi_by">
-                                    <img alt="" class="rounded-circle" src="images/people/4.jpg">
-                                    <span>Thomas, Samsung</span>
-                                </div>
-                            </blockquote>
-                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6" style="background-size: 100%; background-repeat: no-repeat;">
+                    <div class="p-sm-30" style="background-size: 100%; background-repeat: no-repeat;">
+                        <div class="spacer-20 sm-hide"
+                             style="background-size: 100%; background-repeat: no-repeat;"></div>
+                        <p class="lead">جهت مشاوره و راهنمایی تماس بگیرید</p>
+                        <div class="spacer-half" style="background-size: 100%; background-repeat: no-repeat;"></div>
+                        <a class="btn-main" href="tel:09199041290">تماس</a>
+                        <p class="d-small mt20">09199041290 - 09384054988</p>
                     </div>
                 </div>
             </div>
